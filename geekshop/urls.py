dc3ef6767b18f87f072496bc0ee95525d6021238
Sergey Kopeykin <nubobap2015@yandex.ru>
2020-10-03 18:27:59 +0300

    промежуточный коммит, для спокойствия

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -21,7 +21,7 @@ import mainapp.views as mainapp
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", mainapp.main),
-    path("product/", mainapp.products),
-    path("contact/", mainapp.contact),
+    path("", mainapp.main, name="main"),
+    path("product/", mainapp.products, name="products"),
+    path("contact/", mainapp.contact, name="contact"),
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,15 +1,34 @@
+import datetime
 from django.shortcuts import render
+
 
 # Create your views here.
 
 def main(request):
-   return render(request, "mainapp/index.html")
+    title = "Главная"
+    products = [
+        {
+            'name': 'Отличный стул',
+            'desc': 'Расположи седалище комфортно',
+            'image_src': 'product-1.jpg',
+            'image_href': '/product/1', #Почему "/product/1", а не "/products/1"?
+            'alt': 'продукт №1',
+        },
+        {
+            'name': 'Стул повышенного качества',
+            'desc': 'НЕ ОТОРВАТЬСЯ',
+            'image_src': 'img/product-1.jpg',
+            'image_href': '/product/1',
+            'alt': '',
+        },
+    ]
+    content = {"myTitle": title, "products": products}
+    return render(request, "mainapp/index.html", content)
+
 
 def products(request):
-   return render(request, "mainapp/products.html")
+    return render(request, "mainapp/products.html")
+
 
 def contact(request):
-   return render(request, "mainapp/contact.html")
-
-
-
+    return render(request, "mainapp/contact.html")
