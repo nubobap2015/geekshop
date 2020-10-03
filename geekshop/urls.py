7862ae4b11552051f3d1d66d31b051500e2412d3
Sergey Kopeykin <nubobap2015@yandex.ru>
2020-10-03 22:58:15 +0300

    Lesson_2. Using templates... a little bit

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -23,5 +23,10 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("", mainapp.main, name="main"),
     path("product/", mainapp.products, name="products"),
+    path("products/all", mainapp.products, name="products_all"),
+    path("products/home", mainapp.products, name="products_home"),
+    path("products/office", mainapp.products, name="products_office"),
+    path("products/modern", mainapp.products, name="products_modern"),
+    path("products/classic", mainapp.products, name="products_classic"),
     path("contact/", mainapp.contact, name="contact"),
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -11,15 +11,15 @@ def main(request):
             'name': 'Отличный стул',
             'desc': 'Расположи седалище комфортно',
             'image_src': 'product-1.jpg',
-            'image_href': '/product/1', #Почему "/product/1", а не "/products/1"?
+            'image_href': '/product/1',  # Почему "/product/1", а не "/products/1"?
             'alt': 'продукт №1',
         },
         {
-            'name': 'Стул повышенного качества',
-            'desc': 'НЕ ОТОРВАТЬСЯ',
-            'image_src': 'img/product-1.jpg',
-            'image_href': '/product/1',
-            'alt': '',
+            'name': 'Стул с клеевым диспенсером',
+            'desc': 'НЕ ОТОРВАТЬСЯ!',
+            'image_src': 'product-2.jpg',
+            'image_href': '/product/2',
+            'alt': 'продукт №2',
         },
     ]
     content = {"myTitle": title, "products": products}
@@ -27,8 +27,40 @@ def main(request):
 
 
 def products(request):
-    return render(request, "mainapp/products.html")
+    title = "Продукты"
+    links_menu = [
+        {"href": "products_all", "name": "все"},
+        {"href": "products_home", "name": "дом"},
+        {"href": "products_office", "name": "офис"},
+        {"href": "products_modern", "name": "модерн"},
+        {"href": "products_classic", "name": "классика"},
+    ]
+    same_products = [
+        {"name": "Отличный стул", "desc": "Не оторваться.", "image_src": "product-11.jpg", "alt": "продукт 11"},
+        {"name": "Стул повышенного качества", "desc": "Комфортно.", "image_src": "product-21.jpg", "alt": "продукт 21"},
+        {"name": "Стул премиального качества", "desc": "Просто попробуйте.", "image_src": "product-31.jpg", "alt": "продукт 31"},
+    ]
+    content = {"myTitle": title, "links_menu": links_menu, "same_products":same_products}
+    return render(request, "mainapp/products.html", content)
 
 
 def contact(request):
-    return render(request, "mainapp/contact.html")
+    title = "о нас"
+    visit_date = datetime.datetime.now()
+    locations = [
+        {"city": "Москва", "phone": "+7-888-888-8888", "email": "info@geekshop.ru", "address": "В пределах МКАД"},
+        {
+            "city": "Екатеринбург",
+            "phone": "+7-777-777-7777",
+            "email": "info_yekaterinburg@geekshop.ru",
+            "address": "Близко к центру",
+        },
+        {
+            "city": "Владивосток",
+            "phone": "+7-999-999-9999",
+            "email": "info_vladivostok@geekshop.ru",
+            "address": "Близко к океану",
+        },
+    ]
+    content = {"myTitle": title, "visit_date": visit_date, "locations": locations}
+    return render(request, "mainapp/contact.html", content)
