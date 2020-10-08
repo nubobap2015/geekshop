ccc1c0c2afda8d1653fe070d3dbffdd331c5a021
Sergey <61868182+nubobap2015@users.noreply.github.com>
2020-10-08 19:54:14 +0300

    Lesson 2 (#2)
    * промежуточный коммит, для спокойствия
    
    * Lesson_2. Using templates... a little bit

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -21,7 +21,12 @@ import mainapp.views as mainapp
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", mainapp.main),
-    path("product/", mainapp.products),
-    path("contact/", mainapp.contact),
+    path("", mainapp.main, name="main"),
+    path("product/", mainapp.products, name="products"),
+    path("products/all", mainapp.products, name="products_all"),
+    path("products/home", mainapp.products, name="products_home"),
+    path("products/office", mainapp.products, name="products_office"),
+    path("products/modern", mainapp.products, name="products_modern"),
+    path("products/classic", mainapp.products, name="products_classic"),
+    path("contact/", mainapp.contact, name="contact"),
 ]
