f7d2db56fab4bce8cf288060cedb0c3321c2e6b7
Sergey Kopeykin <nubobap2015@yandex.ru>
2020-10-09 16:44:24 +0300

    Lesson_3. Second Try

--- mainapp/views.py
+++ mainapp/views.py
@@ -13,15 +13,13 @@ def main(request):
     return render(request, "mainapp/index.html", content)
 
 
-def products(request):
+def products(request, pk=None):
     title = "Продукты"
-    links_menu = [
-        {"href": "products_all", "name": "все"}]
-    for menu in ProductsCategory.objects.all():
-        links_menu.append({"href": menu.href, "name": menu.name})
-
+    links_menu = ProductsCategory.objects.all()
     same_products = Product.objects.all()
     content = {"myTitle": title, "links_menu": links_menu, "same_products":same_products, "media_url": settings.MEDIA_URL}
+    if pk:
+        print(f"User select category: {pk}")
     return render(request, "mainapp/products.html", content)
 
 
