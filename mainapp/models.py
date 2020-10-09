7ff932ff0b76fa8d2eb6f07d84cbe54cea656e6d
Sergey Kopeykin <nubobap2015@yandex.ru>
2020-10-09 15:53:56 +0300

    Lesson_3.

--- mainapp/models.py
+++ mainapp/models.py
@@ -2,8 +2,9 @@ from django.db import models
 
 # Create your models here.
 
-class ProductCategories(models.Model):
+class ProductsCategory(models.Model):
     name = models.CharField(verbose_name="Наименование", max_length=255, blank=False)
+    href = models.CharField(verbose_name='Ссылка', max_length=255, blank=True)
     desc = models.CharField(verbose_name='Описание', max_length=8000, blank=True)
     #  Служебные
     isDeleted = models.BooleanField(verbose_name='Удалено', default=False)
@@ -11,21 +12,22 @@ class ProductCategories(models.Model):
     date_upd = models.DateTimeField(verbose_name='Дата изменения', auto_now=True)
 
     def __str__(self):
-        return self.name
+        return f'{self.name}{"(удалено)" if self.isDeleted else ""}'
 
-class Products(models.Model):
-    id_cat = models.ForeignKey(ProductCategories, on_delete=models.PROTECT)
+class Product(models.Model):
+    id_cat = models.ForeignKey(ProductsCategory, on_delete=models.PROTECT)
     name = models.CharField(verbose_name="Наименование", max_length=255, blank=False)
     desc = models.CharField(verbose_name='Описание', max_length=8000, blank=True)
-    img_src = models.ImageField(upload_to='products_images', blank=True)
-    img_href = models.CharField(verbose_name="Наименование", max_length=255, blank=False)
+    image_src = models.ImageField(upload_to='products_images', blank=True)
+    image_href = models.CharField(verbose_name="Ссылка", max_length=255, blank=True)
     price = models.DecimalField(verbose_name='Цена', max_digits=12, decimal_places=2, default=0)
     quantity = models.PositiveIntegerField(verbose_name='Кол-во на складе', default=0)
+    alt = models.CharField(verbose_name='Текст картинки', max_length=8000, blank=True)
     #  Служебные
     isDeleted = models.BooleanField(verbose_name='Удалено', default=False)
     date_create = models.DateTimeField(verbose_name='Дата создания' ,auto_now_add=True)
     date_upd = models.DateTimeField(verbose_name='Дата изменения', auto_now=True)
 
     def __str__(self):
-        return f'{self.name.join}[{self.id_cat.name}]{"(удалено)" if self.isDeleted else ""}'
+        return f'{self.name} [{self.id_cat.name}]{"(удалено)" if self.isDeleted else ""}'
 
