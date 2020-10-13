1bad5f5bd1f6729276935e0e03fe30b4d6cb50f2
Sergey Kopeykin <nubobap2015@yandex.ru>
2020-10-13 15:20:32 +0300

    Lesson_4. Ex.3+4 OK

--- authapp/admin.py
+++ authapp/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
 from .models import ShopUser
 
-admin.site.register(ShopUser)
+
+class ShopUserAdmin(UserAdmin):
+    pass
+
+
+admin.site.register(ShopUser, ShopUserAdmin)
--- authapp/models.py
+++ authapp/models.py
@@ -5,4 +5,4 @@ from django.db import models
 # Create your models here.
 class ShopUser(AbstractUser):
     avatar = models.ImageField(upload_to='users_avatars', blank=True)
-    age = models.PositiveIntegerField(verbose_name='Возраст')
+    age = models.IntegerField(verbose_name='Возраст', blank=True, null=True)
