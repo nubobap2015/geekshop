6ffb5610b4073833a43600ce05f11fe4b725603f
Sergey Kopeykin <nubobap2015@yandex.ru>
2020-10-17 13:10:41 +0300

    Lesson_6. Ex2. Hot product. Same products

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,9 +1,9 @@
 import datetime
+import random
 
 from django.conf import settings
 from django.shortcuts import render, get_object_or_404
 
-from basketapp.models import Basket
 from .models import Product, ProductsCategory, Contact
 
 
@@ -12,13 +12,13 @@ from .models import Product, ProductsCategory, Contact
 def main(request):
     title = "Главная"
     my_products = Product.objects.all()[0:4]
-    content = {"myTitle": title, "products": my_products, "media_url": settings.MEDIA_URL,}
+    content = {"myTitle": title, "products": my_products, "media_url": settings.MEDIA_URL, }
     return render(request, "mainapp/index.html", content)
 
 
 def products(request, pk=None):
     title = "Продукты"
-    links_menu = ProductsCategory.objects.all()
+    links_menu = ProductsCategory.objects.filter(isDeleted=False)
     if pk is not None:
         print(f"User select category: {pk}")
         if pk == 0:
@@ -34,10 +34,12 @@ def products(request, pk=None):
                    "media_url": settings.MEDIA_URL,
                    }
         return render(request, "mainapp/products_list.html", content)
-    same_products = Product.objects.all()
+    hot_product = get_hot_product()
+    same_products = get_same_products(hot_product)
     content = {"myTitle": title,
                "links_menu": links_menu,
-               "same_product": same_products,
+               "hot_product": hot_product,
+               "same_products": same_products,
                "media_url": settings.MEDIA_URL,
                }
     return render(request, "mainapp/products.html", content)
@@ -50,3 +52,11 @@ def contact(request):
     content = {"myTitle": title, "visit_date": visit_date, "locations": locations, "media_url": settings.MEDIA_URL,
                }
     return render(request, "mainapp/contact.html", content)
+
+
+def get_hot_product():
+    return random.sample(list(Product.objects.all()), 1)[0]
+
+
+def get_same_products(hot_product):
+    return Product.objects.filter(id_cat=hot_product.id_cat).exclude(pk=hot_product.pk)[:3]
