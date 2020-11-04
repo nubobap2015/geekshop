4acbe745d8eaab392d0943d94d89dbf83141fa98
Sergey Kopeykin <nubobap2015@yandex.ru>
2020-11-04 17:18:25 +0300

    lesson_2.1. Context processor на связи

--- mainapp/views.py
+++ mainapp/views.py
@@ -5,7 +5,7 @@ from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.shortcuts import get_object_or_404, render
 from django.utils import timezone
 
-from basketapp.models import Basket
+# from basketapp.models import Basket
 
 from .models import Contact, Product, ProductCategory
 
@@ -16,13 +16,13 @@ def main(request):
     content = {"title": title, "products": products, "media_url": settings.MEDIA_URL}
     return render(request, "mainapp/index.html", content)
 
-
+'''
 def get_basket(user):
     if user.is_authenticated:
         return Basket.objects.filter(user=user)
     else:
         return []
-
+'''
 
 def get_hot_product():
     products = Product.objects.filter(is_active=True, category__is_active=True)
@@ -37,7 +37,7 @@ def get_same_products(hot_product):
 def products(request, pk=None, page=1):
     title = "продукты"
     links_menu = ProductCategory.objects.filter(is_active=True)
-    basket = get_basket(request.user)
+    # basket = get_basket(request.user)
 
     if pk is not None:
         if pk == '0':
@@ -63,7 +63,7 @@ def products(request, pk=None, page=1):
             "category": category,
             "products": products_paginator,
             "media_url": settings.MEDIA_URL,
-            "basket": basket,
+            # "basket": basket,
         }
         return render(request, "mainapp/products_list.html", content)
     hot_product = get_hot_product()
@@ -73,7 +73,7 @@ def products(request, pk=None, page=1):
         "links_menu": links_menu,
         "same_products": same_products,
         "media_url": settings.MEDIA_URL,
-        "basket": basket,
+        # "basket": basket,
         "hot_product": hot_product,
     }
     return render(request, "mainapp/products.html", content)
@@ -85,7 +85,7 @@ def product(request, pk):
         "title": title,
         "links_menu": ProductCategory.objects.filter(is_active=True),
         "product": get_object_or_404(Product, pk=pk),
-        "basket": get_basket(request.user),
+        # "basket": get_basket(request.user),
         "media_url": settings.MEDIA_URL,
     }
     return render(request, "mainapp/product.html", content)
